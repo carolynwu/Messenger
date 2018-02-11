4a28c109d514afe7d819a473c3691f5c92bd4647
carolynwu <wuxujia199438@gmail.com>
2018-02-10 20:33:37 -0600

    binding

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import {FormsModule} from "@angular/forms";
 
 // decorator
 @NgModule({
     declarations: [
         AppComponent
     ],
-    imports: [BrowserModule],
+    imports: [BrowserModule,FormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
