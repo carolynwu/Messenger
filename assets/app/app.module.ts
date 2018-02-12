9b4ac7b720ad828ebab6431c9cc9a502eea1fa0e
carolynwu <wuxujia199438@gmail.com>
2018-02-12 10:48:11 -0600

    data binding

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
 import {FormsModule} from "@angular/forms";
+import {MessageComponent} from "./messages/message.component";
 
 // decorator
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MessageComponent
     ],
     imports: [BrowserModule,FormsModule],
     bootstrap: [AppComponent]
