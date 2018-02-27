84f11ba587515bcc973eeb05af90c14fc370c03b
carolynwu <wuxujia199438@gmail.com>
2018-02-27 09:07:56 -0600

    get message with a service

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,6 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 import{ Message} from "./message.model";
+import {MessageService} from "./message.service";
 
 @Component({
     selector: 'app-message-list',
@@ -12,13 +13,14 @@ import{ Message} from "./message.model";
                     (editClicked)="message.content=$event"
              *ngFor="let message of messages"> </app-message>
         </div> 
-    `
+    `,
+    providers:[MessageService]
 })
-export class MessageListComponent{
-    messages: Message[]=[
-        new Message('Some message','carolyn'),
-        new Message('Something else', 'carolyn'),
-        new Message('lalala', 'carolyn')
-    ];
+export class MessageListComponent implements OnInit{
+    messages: Message[];
+    constructor(private messageService: MessageService){}
 
+    ngOnInit(){
+        this.messages=this.messageService.getMessage();
+    }
 }
