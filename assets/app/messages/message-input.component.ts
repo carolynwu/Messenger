a7454e6c627e1a31eb932cc737536d5dbe910af4
carolynwu <wuxujia199438@gmail.com>
2018-02-28 11:03:36 -0600

    message with a template-driven form
    the form reset, avoid submit the empty form

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {MessageService} from "./message.service";
 import {Message} from "./message.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
     selector:'app-message-input',
@@ -11,9 +12,11 @@ export class MessageInputComponent{
     constructor(private messageService: MessageService){}
 
 
-    onSave(value:string){
-        const message=new Message(value, "carolyn");
+    onSubmit(form:NgForm){
+     //console.log(form);
+        const message=new Message(form.value.content, "carolyn");
         this.messageService.addMessage(message);
+        form.resetForm();
 
     }
 }
