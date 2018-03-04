d9945e7255c927ec306b37d1e9d99ebb5871c047
carolynwu <wuxujia199438@gmail.com>
2018-03-03 18:14:43 -0600

    using sign up data-driven form
    make submit work only when all inputs are true

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -8,7 +8,6 @@ const APP_ROUTES:Routes=[
 { path:'', redirectTo:'/messages',pathMatch:'full'},
 { path:'messages', component:MessagesComponent},
 { path:'auth', component:AuthenticationComponent,children:AUTH_ROUTES}
-
 ];
 
 export const routing=RouterModule.forRoot(APP_ROUTES);
