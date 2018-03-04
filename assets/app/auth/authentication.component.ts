ecb3121481c2137d22734f1becabb91a8bcb9306
carolynwu <wuxujia199438@gmail.com>
2018-03-03 18:35:04 -0600

    finish signin and delete edit works

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -6,9 +6,9 @@ import {Component} from "@angular/core";
             <header class="row spacing">
                 <nav class="col-md-8 col-md-offset-2">
                     <ul class="nav nav-tabs">
-                        <li routerActive="active"><a [routerLink]="['signup']"> Signup</a></li>
-                        <li routerActive="active"><a [routerLink]="['signin']"> Signin</a></li>
-                        <li routerActive="active"><a [routerLink]="['logout']"> Logout</a></li>
+                        <li routerLinkActive="active"><a [routerLink]="['signup']"> Signup</a></li>
+                        <li routerLinkActive="active"><a [routerLink]="['signin']"> Signin</a></li>
+                        <li routerLinkActive="active"><a [routerLink]="['logout']"> Logout</a></li>
                     </ul>
                 </nav>
             </header>
