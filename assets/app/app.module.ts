30aa57095fc022d593c164eab1c6098c877074e3
carolynwu <wuxujia199438@gmail.com>
2018-03-05 08:23:56 -0600

    bug! save message from fronted end to back end
    observations in http

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import{HttpModule} from "@angular/http";
 import {MessageComponent} from "./messages/message.component";
 import {MessageListComponent} from "./messages/message-list.component";
 import{ MessageInputComponent} from "./messages/message-input.component";
@@ -29,7 +30,12 @@ import {SigninComponent} from "./auth/signin.component";
         SignupComponent,
         SigninComponent
     ],
-    imports: [BrowserModule,FormsModule, routing,ReactiveFormsModule],
+    imports: [
+            BrowserModule,
+            FormsModule,
+            routing,
+            ReactiveFormsModule,
+            HttpModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
