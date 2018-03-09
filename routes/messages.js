08ba1a774289f89375e5ea5a1c4d831b8c173120
carolynwu <wuxujia199438@gmail.com>
2018-03-09 12:15:06 -0600

    patch route+actuallt get the message, check the erros changed message back to the database

--- routes/messages.js
+++ routes/messages.js
@@ -41,5 +41,36 @@ router.post('/', function (req, res, next) {
     });
 });
 
-
+//patch change existing data
+router.patch('/:id',function (req,res,next) {
+    Message.findById(req.params.id,function(err,message){
+        if(err){
+            return res.status(500).json({
+                title:"An error occured",
+                error:err
+            });
+        }
+        if(!message){
+            return res.status(500).json({
+                title:"No Message Found!",
+                error:{message:"Message not found"}
+            });
+    }
+    //save or update message
+        message.content=req.body.content;
+        message.save(function(err, result){
+            if(err){
+                return res.status(500).json({
+                    title:"An erroe occured",
+                    error:err
+                });
+            }
+            //if the error is null
+            res.status(200).json({
+                message:"updated message",
+                obj:result
+            });
+        });
+    });
+});
 module.exports = router;
