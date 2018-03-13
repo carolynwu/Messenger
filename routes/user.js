06ded364e8c89a93a5d8b67e0fcde38b00c619a5
carolynwu <wuxujia199438@gmail.com>
2018-03-13 16:51:39 -0500

    sign in-jwt-backend
    the bcrypt package we used for encrypting give a hash

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt=require("bcryptjs");
-
+var jwt = require('jsonwebtoken');
 var User=require('../models/user');
 //create a user
 router.post('/', function (req, res, next) {
@@ -25,5 +25,33 @@ router.post('/', function (req, res, next) {
     });
 });
 
+router.post("./signin",function(req,res,next){
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            return res.status(500).json({
+                title:"An error occured",
+                error:err
+            });
+        }
+        if(!user){
+            return res.status(401).json({
+                title:"Login failed",
+                error:{message:"Invalid login credentials"}
+            });
+        }
+       if(!bcrypt.compareSync(req.body.password, user.password)){
+           return res.status(401).json({
+               title:"Login failed",
+               error:{message:"Invalid login credentials"}
+           });
+       }
+        var token = jwt.sign({ user:user}, 'secret',{expireIn:7200});
+        res.status(200).json({
+            message:"Successfully logged in",
+            token:token,
+            userId:user._id
+        });
+    });
+});
 
 module.exports = router;
