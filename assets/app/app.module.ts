2d767a92df942213fe90533cd2143bf6186a1356
carolynwu <wuxujia199438@gmail.com>
2018-03-13 10:55:58 -0500

    sign up users-connect backed and frontend
    create new file, injectable,observable
    JSON.stringify() method converts a JavaScript value to a JSON string

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -14,6 +14,7 @@ import {routing} from "./app.routing";
 import {LogoutComponent} from "./auth/logout.component";
 import {SignupComponent} from "./auth/signup.component";
 import {SigninComponent} from "./auth/signin.component";
+import {AuthService} from "./auth/auth.service";
 
 
 // decorator
@@ -35,7 +36,9 @@ import {SigninComponent} from "./auth/signin.component";
             FormsModule,
             routing,
             ReactiveFormsModule,
-            HttpModule],
+            HttpModule
+    ],
+    providers:[AuthService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
